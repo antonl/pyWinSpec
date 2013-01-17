395a5c528e65a59e43944c1d87d9da090e8c91b7
Anton Loukianov <anton.loukianov@gmail.com>
2013-01-16 23:23:46 -0500

    fix the transition to packaging, typos

--- winspec/__init__.py
+++ winspec/__init__.py
@@ -1,19 +1,21 @@
 from structs import Header, AxisCalibration, ROIinfo
+from io import FileIO
+from os import path
 
-if __name__ == '__main__':
+def print_headers():
     import inspect, re, io
 
     with io.FileIO('02_reference.SPE', mode='r') as f:
         A = Header()
         f.readinto(A)
         
-        for i in [WinSpecHeader, AxisCalibration, ROIinfo]:
+        for i in [Header, AxisCalibration, ROIinfo]:
             fields = []
 
             print '{:20s}[{:4s}]\toffset'.format(str(i), 'size')
             
             for name,obj in inspect.getmembers(i):
-                if inspect.isdatadescriptor(obj) and not inspect.ismemberdescriptor(obj) 
+                if inspect.isdatadescriptor(obj) and not inspect.ismemberdescriptor(obj) \
                     and not inspect.isgetsetdescriptor(obj):
                     
                     fields.append((name, obj))
@@ -24,3 +26,13 @@ if __name__ == '__main__':
             for name, obj in fields:
                 print '{:20s}[{:3d}]\toff={:4d}'.format(name, obj.size, obj.offset)
 
+class SpeFile(object):
+    def __init__(self, name):
+        with FileIO(name, mode='r') as f:
+            self.header = Header()
+            self.path = path.realpath(name) 
+            f.readinto(self.header)
+
+    def __repr__(self):
+        return 'SPE File {:s}\n\t{:d}x{:d} area, {:d} frames\n\tTaken on {:s}' \
+                .format(self.path, self.header.xdim, self.header.ydim, self.header.NumFrames, self.header.date)
